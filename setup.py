2d778a76a0b7ae9adb941d0ef28c6258c3dc9054
Daniel Rust <dlrust@gmail.com>
2012-04-05 17:30:46 -0700

    Working on a setup py

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='phonenumbers',
+    version='4.7b1',
+    description="Python version of Google's common library for parsing, formatting, storing and validating international phone numbers.",
+    author='David Drysdale',
+    author_email='dmd@lurklurk.org',
+    url='https://github.com/daviddrysdale/python-phonenumbers',
+    license='Apache License 2.0',
+    #packages=['phonenumbers', 'phonenumbers.data', 'phonenumbers.geodata'],
+    packages=find_packages(),
+    test_suite="tests",
+    platforms='Posix; MacOS X; Windows',
+    classifiers=[dev_status,
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Communications :: Telephony',
+    ],
+)
